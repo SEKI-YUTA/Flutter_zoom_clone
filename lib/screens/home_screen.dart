127191211db9101aabee5dd892393870b3a13129
SEKI-YUTA <zard.since1991@gmail.com>
2022-03-22 17:31:18 +0900

    finsihed around 1:20:00

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:zoom_clone/utils/colors.dart';
+import 'package:zoom_clone/widgets/home_meeting_button.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -8,10 +10,66 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  int _currentPage = 0;
+  onPageChanged(int page) {
+    setState(() {
+      _currentPage = page;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        title: const Text('Meet & Chat'),
+        centerTitle: true,
+        backgroundColor: backgroundColor,
+      ),
+      body: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              HomeMeetingButton(
+                onPressed: () {},
+                text: "New Meeting",
+                icon: Icons.videocam,
+              ),
+              HomeMeetingButton(
+                onPressed: () {},
+                text: "Join Meeting",
+                icon: Icons.add_box_rounded,
+              ),
+              HomeMeetingButton(
+                onPressed: () {},
+                text: "Schedule",
+                icon: Icons.calendar_today,
+              ),
+              HomeMeetingButton(
+                onPressed: () {},
+                text: "Share screen",
+                icon: Icons.arrow_upward_rounded,
+              ),
+            ],
+          ),
+          const Expanded(
+              child: Center(
+            child: Text(
+              'Create/Join meetings with just a click',
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+            ),
+          ))
+        ],
+      ),
       bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: footerColor,
+        selectedItemColor: Colors.white,
+        unselectedItemColor: Colors.grey,
+        onTap: onPageChanged,
+        currentIndex: _currentPage,
+        type: BottomNavigationBarType.fixed,
+        unselectedFontSize: 14,
         items: const [
           BottomNavigationBarItem(
               icon: Icon(Icons.comment_bank), label: "Meet & Char"),
