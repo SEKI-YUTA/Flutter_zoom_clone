598c3511b07d2bde528feab372ade0af010a7c72
SEKI-YUTA <zard.since1991@gmail.com>
2022-03-22 09:19:02 +0900

    view: add bottomNavigationBar on HomeScreen.dart

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -10,6 +10,21 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(
+              icon: Icon(Icons.comment_bank), label: "Meet & Char"),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.lock_clock), label: "Meetings"),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.person_outline), label: "Contacts"),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.settings), label: "Settings"),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.comment_bank), label: "Meet & Char"),
+        ],
+      ),
+    );
   }
 }
